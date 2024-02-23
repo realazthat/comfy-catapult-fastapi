ededd0365df627e48344490f9753459012962e37
Azriel Fasten <realazthat@gmail.com>
2024-02-23 13:21:44 -0500

    Use Field to document schema.

--- src/api_schema.py
+++ src/api_schema.py
@@ -5,7 +5,7 @@
 # under the MIT license or a compatible open source license. See LICENSE.md for
 # the license text.
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Lora(BaseModel):
@@ -15,40 +15,22 @@ class Lora(BaseModel):
 
 
 class SimpleWorkflowRequest(BaseModel):
-  job_id: str
-  """The job_id of the workflow."""
-  positive_prompt: str
-  """The postive prompt."""
-  negative_prompt: str
-  """The negative prompt."""
-  ckpt_name: str
-  """The model."""
+  job_id: str = Field(..., description='The job_id of the workflow.')
+  positive_prompt: str = Field(..., description='The postive prompt.')
+  negative_prompt: str = Field(..., description='The negative prompt.')
+  ckpt_name: str = Field(..., description='The checkpoint name.')
   # TODO: This should be a list.
-  lora: Lora
-  """List of LoRas to apply to model."""
-
-  hires: bool
-  """High resolution toggle."""
-
-  seed: int
-  """The seed value."""
-
-  cfg: float
-  """The Stable Diffusion cfg value."""
-  denoise: float
-  """The Stable Diffusion denoise value."""
-
-  steps: int
-  """The number of Stable Diffusion steps."""
-
-  width: int
-  """The width of the image."""
-  height: int
-  """The height of the image."""
+  lora: Lora = Field(..., description='List of LoRas to apply to model.')
+  hires: bool = Field(..., description='High resolution toggle.')
+  seed: int = Field(..., description='The seed value.')
+  cfg: float = Field(..., description='The Stable Diffusion cfg value.')
+  denoise: float = Field(..., description='The Stable Diffusion denoise value.')
+  steps: int = Field(..., description='The number of Stable Diffusion steps.')
+  width: int = Field(..., description='The width of the image.')
+  height: int = Field(..., description='The height of the image.')
 
 
 class SimpleWorkflowResponse(BaseModel):
-  job_id: str
-  """The job_id of the workflow."""
-  output_b64: str
-  """The base64 encoded output of the workflow."""
+  job_id: str = Field(..., description='The job_id of the workflow.')
+  output_b64: str = Field(
+      ..., description='The base64 encoded output of the workflow.')
